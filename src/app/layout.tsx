fb666d63742550f199c668037036c0addfe8ccf7
umar khitab <umair123123ss@gmail.com>
2025-02-08 12:26:14 +0500

    Update layout metadata title and description for improved clarity and detail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,9 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "umar",
-  description: "this is me umar khitab ",
+  title: "Umar - MERN Stack Developer",
+  description:
+    "I am Umar Khitab, a passionate MERN stack developer specializing in building dynamic, high-performance web applications. I have expertise in MongoDB, Express.js, React.js, and Node.js, creating full-stack solutions that are scalable, efficient, and user-friendly. My focus is on crafting clean code, optimizing performance, and delivering seamless user experiences through modern web technologies. With a strong foundation in front-end and back-end development, I transform ideas into fully functional web applications. Let's build something amazing together!",
 };
 
 export default function RootLayout({
